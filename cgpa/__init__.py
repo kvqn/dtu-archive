0b9da42de585af047a562c709f3eaa5f06402e1d
kevqn11 <guneetaggarwal@gmail.com>
2022-11-26 00:24:32 +0530

    declutter.py

---
+++ cgpa/__init__.py
@@ -0,0 +1,7 @@
+
+class Paths:
+    PDF = "O21_BTECH_I_1446.pdf"
+    TEXT = "new_text.txt"
+    TEXT_DECLUTTERED = "new_text_decluttered.txt"
+
+from .declutter import declutter
---
+++ cgpa/declutter.py
@@ -0,0 +1,34 @@
+from . import Paths
+import re
+
+REMOVE_REGEXES = [
+    r"^ *Delhi +Technological +University *$",
+    r"^ *\(.*$",
+    r"^ *\( *Formerly +Delhi +College +of +Engineering *\) *$",
+    r"^ *Regular +Result +Notification *$",
+    r"^ *THE +RESULT.*$",
+    r"^ *Program.*$",
+    r"^ *Branch.*$",
+    r"^ *[A-Z]+[0-9]* +:.*$",
+    r"^ *Credits.*$",
+    r"^ *Any +discrepancy.*$",
+    r"^ *Digitally +Signed.*$",
+    r"^ *Controller.*$",
+    r"^ *Date.*$",
+    r"^ *valid +only.*$",
+    r"^ *Visit.*$",
+    r"^ *Sem +:.*$",
+    r"^ *PRESENTATION +SKILLS.*$",
+]
+
+def declutter():
+    with open(Paths.TEXT, "r") as file:
+        with open(Paths.TEXT_DECLUTTERED, "w") as file2:
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                for regex in REMOVE_REGEXES:
+                    line = re.sub(regex, "", line)
+                file2.write(line)
+        
