f98bc7ca382b8b492b20ddf8a848316f1c6e5e74
kvqn <guneetaggarwal@gmail.com>
2022-11-26 19:20:44 +0530

    cleaner output : extract_text.py

--- cgpa/extract_text.py
+++ cgpa/extract_text.py
@@ -1,11 +1,19 @@
 import pdfplumber
+import sys
 
 def extract_text(pdf_path : str, output_path : str):
     with pdfplumber.open(pdf_path) as pdf:
         with open(output_path, "w") as file:
             i = 0
+            total_pages = len(pdf.pages)
             for page in pdf.pages:
                 i+=1
-                print(i)
+                sys.stdout.write(f"\rProcessing Page [{i}/{total_pages}]")
+                sys.stdout.flush()
+                # print(i)
                 text = page.extract_text(layout=True, y_density=10)
                 file.write(text)
+            sys.stdout.write(f"\rProcessed Pages [{total_pages}/{total_pages}]\n")
+        sys.stdout.write(f"Successfully extracted text from PDF. Next step is to declutter.\n")
+        sys.stdout.write(f"cgpa declutter {output_path}\n")
+        sys.stdout.flush()
