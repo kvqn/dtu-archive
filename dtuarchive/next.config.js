a21e400d2db500e23ac109a9cebedcda70ab7a8f
kvqn <guneetaggarwal@gmail.com>
2023-07-06 22:21:17 +0530

    increased timeout

--- dtuarchive/next.config.js
+++ dtuarchive/next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   env: {
     baseurl: "http://127.0.0.1:3000"
-  }
+  },
   // webpack(config) {
   //   config.infrastructureLogging = { debug: /PackFileCache/ }
   //   return config;
   // }
+  staticPageGenerationTimeout: 120
 }
 
 module.exports = nextConfig
