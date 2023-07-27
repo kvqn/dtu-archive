c11f8e20318fbe943d5fc87069b7ef04506faf5d
kvqn <guneetaggarwal@gmail.com>
2023-07-27 12:27:08 +0530

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import { Analytics } from "@vercel/analytics/react"
 import { AppProps } from "next/app"
-
 import "../styles/globals.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="h-screen">
       <Component {...pageProps} />
-      <Analytics />
     </div>
   )
 }
