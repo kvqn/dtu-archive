075128a730ffd193a1cb1c5fbcc0dc1d612c3c92
kvqn <guneetaggarwal@gmail.com>
2024-03-22 04:38:19 +0530

    build: changed the names of some environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,14 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DTUARCHIVE_MARIADB_HOST: string;
-      DTUARCHIVE_MARIADB_USER: string;
-      DTUARCHIVE_MARIADB_PASS: string;
-      DTUARCHIVE_MARIADB_PORT: string;
-      DTUARCHIVE_MARIADB_DB: string;
-      NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
-      GITHUB_ID: string;
-      GITHUB_SECRET: string;
+      MARIADB_HOST: string
+      MARIADB_USER: string
+      MARIADB_PASS: string
+      MARIADB_PORT: string
+      MARIADB_DB: string
+      NEXT_PUBLIC_GA_MEASUREMENT_ID: string
+      GITHUB_ID: string
+      GITHUB_SECRET: string
     }
   }
 }
