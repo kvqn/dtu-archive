9c8227fa5af3ac3fc5860ca58c001b7a48504970
kvqn <guneetaggarwal@gmail.com>
2024-04-02 20:14:15 +0530

    fix: fixed build issues

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
   //   return config;
   // }
   reactStrictMode: true,
-  staticPageGenerationTimeout: 120,
+  staticPageGenerationTimeout: 300,
   images: {
     remotePatterns: [
       {
--- src/server/getAggregateResult.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import { getAggregateResult as _getAggregateResult } from "@/lib/data"
-
-export async function getAggregateResult(batch: string, branch: string) {
-  return await _getAggregateResult(batch, branch)
-}
