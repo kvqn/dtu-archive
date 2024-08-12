bf487f495064a48995859bbf01062aef425d1497
kvqn <guneetaggarwal@gmail.com>
2024-08-13 00:06:57 +0530

    style: /student page

--- src/app/student/client.tsx
+++ src/app/student/client.tsx
@@ -1,11 +1,26 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import type { Student } from "@/lib/data/getAllStudents"
-import { rollBreakdown } from "@/lib/utils"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { Suspense, useState } from "react"
+
+function rollBreakdown(rollno: string): {
+  batch: string
+  branch: string
+  roll: number
+} {
+  const match = rollno.match(/(2K\d+)\/([A-Z0-9]+)\/(\d+)/)
+  if (!match) throw new Error("Invalid rollno")
+
+  return {
+    batch: match[1],
+    branch: match[2],
+    roll: parseInt(match[3]),
+  }
+}
 
 function rollnoToUrl(rollno: string) {
   const { batch, branch, roll } = rollBreakdown(rollno)
@@ -13,9 +28,10 @@ function rollnoToUrl(rollno: string) {
 }
 
 export function SearchStudent({ students }: { students: Student[] }) {
-  const [filter, setFilter] = useState("")
+  let filter = ""
   const [filteredStudents, setFilteredStudents] = useState<typeof students>([])
-  useEffect(() => {
+
+  function submit() {
     if (filter === "") return setFilteredStudents([])
     setFilteredStudents(
       students.filter(
@@ -25,27 +41,31 @@ export function SearchStudent({ students }: { students: Student[] }) {
           student.new?.toLowerCase().includes(filter.toLowerCase())
       )
     )
-  }, [filter, students])
+  }
+
   return (
     <div className="flex w-[80%] flex-col items-center gap-4">
-      <div className="flex w-full items-center gap-2 rounded-md border border-black bg-white p-2">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      <div className="flex w-full items-center gap-2 rounded-md border p-2">
+        <FontAwesomeIcon icon={faMagnifyingGlass} height={"1rem"} />
         <input
-          className="font-geist text-xl outline-none"
-          onChange={(e) => setFilter(e.target.value)}
+          autoFocus
+          onKeyDown={(e) => e.key === "Enter" && submit()}
+          className="w-full text-xl outline-none dark:bg-black"
+          onChange={(e) => (filter = e.target.value)}
         />
+        <Button onClick={submit}>Search</Button>
       </div>
       <div className="flex w-full flex-col gap-4">
         {filteredStudents.map((student) => (
           <Link
             href={rollnoToUrl(student.new ?? student.old)}
             key={student.old}
-            className="flex h-[80px] items-center divide-x divide-black overflow-hidden rounded-md border border-black bg-white font-geist shadow-lg"
+            className="font-geist flex h-[80px] items-center divide-x overflow-hidden rounded-md border shadow-lg transition-all hover:scale-105"
           >
-            <div className="whitespace-preline flex h-full flex-grow items-center bg-emerald-100 p-2 px-4 font-medium">
+            <div className="whitespace-preline flex h-full flex-grow items-center bg-neutral-100 p-2 px-4 font-medium dark:bg-neutral-900">
               {student.name}
             </div>
-            <div className="flex h-full min-w-[40%] flex-col items-center justify-center whitespace-nowrap bg-emerald-300 p-2 font-geist-mono">
+            <div className="300 flex h-full min-w-[40%] flex-col items-center justify-center whitespace-nowrap bg-neutral-50 p-2 font-geist-mono dark:bg-neutral-950">
               <p>{student.new}</p>
               <p>{student.old}</p>
             </div>
--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,13 +1,11 @@
 import { getAllStudents } from "@/lib/data/getAllStudents"
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import { SearchStudent } from "./client"
 
 export default async function Page() {
   const students = await getAllStudents()
   return (
-    <div className="flex w-full flex-col items-center gap-6">
+    <div className="flex w-full flex-col items-center gap-6 pt-16">
       <p className="font-geist text-xl font-bold">
         View individual student records
       </p>
