622f471386a308302d95db2c17ee3151b9fb9ba0
kvqn <guneetaggarwal@gmail.com>
2024-09-22 12:53:17 +0530

    Revert "feat: use query params in archive"
    This reverts commit 00ed241782485ccc2e136bcd3a8442cdd73431c7.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,14 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
-import NextAdapterApp from "next-query-params/app"
 import { ThemeProvider } from "next-themes"
-import { QueryParamProvider } from "use-query-params"
 
 export default function ContextProvider({ children, session }: any) {
   return (
     <SessionProvider session={session}>
       {/* @ts-ignore */}
       <ThemeProvider defaultTheme="light" attribute="class">
-        <QueryParamProvider adapter={NextAdapterApp}>
-          {children}
-        </QueryParamProvider>
+        {children}
       </ThemeProvider>
     </SessionProvider>
   )
